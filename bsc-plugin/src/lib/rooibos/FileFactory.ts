54ad80dae11857b7aecca2f3f404966edd4db17d
Charles Ewert <cewert@gmail.com>
2023-03-27 09:00:44 -0400

    test

--- bsc-plugin/src/lib/rooibos/FileFactory.ts
+++ bsc-plugin/src/lib/rooibos/FileFactory.ts
@@ -1,4 +1,8 @@
-import { type BrsFile, type Program, type XmlFile, standardizePath as s } from 'brighterscript';
+import {
+    type BrsFile ,
+    type Program ,
+    type XmlFile ,
+    standardizePath as s } from 'brighterscript';
 import * as path from 'path';
 import * as fs from 'fs';
 
